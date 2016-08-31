15b6cafb2116353a3e0c1bb9216841f161a19605
nukosuke <nukosuke@shibuya.tech>
2016-09-01 04:10:23 +0900

    React server side rendering

--- src/server/app.tsx
+++ src/server/app.tsx
@@ -1,14 +1,29 @@
 import * as express from 'express';
-import User from './services/models/user';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/server';
+import App from '../client/App';
 
 const app = express();
 
+// provide static files
+app.use(express.static('public'));
+
+// routing for react server-side rendering
 app.get('/', (req: express.Request, res: express.Response) => {
-  User
-  .fetchAll()
-  .then(users => {
-    res.send(users);
-  });
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>react</title>
+      </head>
+      <body>
+        <div id='app'>`
+          + ReactDOM.renderToString(<App/>) +
+       `</div>
+        <script src='/assets/javascripts/app.js'></script>
+      </body>
+    </html>
+  `);
 });
 
 export default app;
