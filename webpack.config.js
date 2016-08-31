1c7413348a09e4efb2dc37b70cbe138aed8633e4
nukosuke <nukosuke@shibuya.tech>
2016-09-01 02:28:13 +0900

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  entry: {
+    app: './src/client/App.tsx'
+  },
+  output: {
+    path: __dirname + '/public/assets/javascripts',
+    filename: '[name].js'
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['', '.ts', '.tsx', '.js'],
+    modulesDirectories: ['node_modules']
+  },
+  module: {
+    loaders: [
+      { test: /\.tsx?$/, loader: 'ts-loader', exclude: /node_modules/ }
+    ],
+    preLoaders: [
+      { test: /\.js$/, loader: 'source-map-loader' }
+    ]
+  },
+  externals: {
+    'react': 'React',
+    'react-dom': 'ReactDOM'
+  }
+};
